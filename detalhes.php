5ba9aad00ccf4723b949b0df2546b98843baaddc
Junior <aefs12junior@gmail.com>
2016-10-18 22:07:15 -0300

    Projeto

--- detalhes.php
+++ detalhes.php
@@ -1,35 +1,15 @@
 <?php
+
 require_once 'vendor/autoload.php';
-
-/*$teste = new \App\Controller\Teste();
-
-$teste->ola();*/
-
 
 if (isset($_GET['user'])) {
 
     $cliente = new \App\Controller\Github();
     $repositories = $cliente->detalhes($_GET['user']);
 
-    //var_dump($repositories);
     $dados = $repositories[0];
-    // $client = new \Github\Client();
 
-    // $repositories = $client->api('user')->repositories($_GET['user']);
-    // //echo  $_GET['nome']
-    // $dados = $repositories[0];
-    // //echo $_GET['id'];
-    // //echo $dados["owner"]["avatar_url"];
-    // echo "<pre>";
-    // //var_dump($repositories);
-    // var_dump($dados);
-
-
-    // // NOME DO USUARIO
-    //echo $dados["owner"]["html_url"];
-
-    require_once 'App/View/cabecalho.html';
-}
+    require_once 'App/View/cabecalho.html';}
 ?>
 
 <div><img src="<?=$dados["owner"]["avatar_url"]; ?>" alt=""></div>
@@ -43,14 +23,10 @@ if (isset($_GET['user'])) {
     <div class="dados">
     
         <a href="<?=$r["html_url"]?>"><span class="projeto"><?=$r["html_url"]?></span></a>
-        <?php
-            // PROJETOS
-
-            //echo '<a href=""><span class="projeto">'.$r["html_url"].'</span></a>';
-        }
-        ?>
+    <?php
+    }
+    ?>
             
     </div>
 <?php
-    require_once 'App/View/rodape.html'
-    ?>
+require_once 'App/View/rodape.html';
--- index.php
+++ index.php
@@ -1,30 +1,12 @@
 <?php
 
-//https://github.com/KnpLabs/php-github-api
-//https://github.com/KnpLabs/php-github-api/tree/master/doc
-
 require_once 'vendor/autoload.php';
-
-
-//$teste = new \App\Controller\Github();
 
 if (isset($_POST['ok'])) {
 
-    // $client = new \Github\Client();
-    // $repositories = $client->api('user')->find($_POST['pesquisar']);
-    // $dados = $repositories["users"];
-
     $cliente = new \App\Controller\Github();
     $dados = $cliente->lista($_POST['pesquisar']);
-    //var_dump($dados);
 
-
-    //$repositories = $client->api('user')->repositories('junior');
-    //echo "<pre>";
-    //var_dump($t);
-    //print_r($repositories);
-    //var_dump($repositories);
-//var_dump($repositories["name"]);
 }
 
 require_once'App/View/cabecalho.html';
